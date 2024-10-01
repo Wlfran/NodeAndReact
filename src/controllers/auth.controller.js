57d512df04ddd025828490e91a1c1e58b9a0dfbb
Wlfran <wlfranhernandez11@hotmail.es>
2024-10-01 10:56:55 -0500

    Creacion de archivos para el registro de usuarios

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,2 @@
+export const register = (req, res) => res.send("Register"); 
+export const login = (req, res) => res.send("Login");
---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+});
+
+export default mongo.model('User', userSchema);
